0a18818e922b17ceadff4f84022e5e85d6435e33
ilsave <bulldoo@yandex.ru>
2020-03-28 19:10:24 +0300

    add sout
    system out println

--- src/ru/gushin/ngtu/ivt/Main.java
+++ src/ru/gushin/ngtu/ivt/Main.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println();
         // Пробуем запустить все декораторы. Фраза помещается в кавычки и квадратные скобки
         PrinterInterface printer = new QuotesDecorator(new LeftBracketDecorator( new RightBraсketDecorator( new Printer("Hey there !"))));
         // Фраза выводится на экран
